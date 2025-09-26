902e16f43a9c8a9155f157e5a7c3ccdc0ca1bd6c
felipehbomfim <lipe1800@hotmail.com>
2025-09-26 10:53:39 -0300

    alteracoes

--- lib/src/widgets/easy_day_widget/easy_day_widget.dart
+++ lib/src/widgets/easy_day_widget/easy_day_widget.dart
@@ -249,7 +249,7 @@ class EasyDayWidget extends StatelessWidget {
       monthStyle = todayMonthStrStyle;
     }
     return DayInfoText(
-      text: EasyDateFormatter.shortMonthName(date, locale).toUpperCase(),
+      text: EasyDateFormatter.shortMonthName(date, locale),
       textStyle: monthStyle,
     );
   }
@@ -324,7 +324,7 @@ class EasyDayWidget extends StatelessWidget {
       dayStrStyle = todayStrStyle;
     }
     return DayInfoText(
-      text: EasyDateFormatter.shortDayName(date, locale).toUpperCase(),
+      text: EasyDateFormatter.shortDayName(date, locale),
       textStyle: dayStrStyle,
     );
   }
